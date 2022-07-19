2a765c0d623535e2798f697f079a68b986e55fd6
Zedan <zed.saheer5@gmail.com>
2022-07-19 09:14:01 +0530

    Created structure for menu and dashboard , Creating dashboard layout

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "cdn.discordapp.com",
+    ],
+  },
 }
 
 module.exports = nextConfig
