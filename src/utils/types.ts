a6f936342b946f92daa2b0801350fe56a1938a08
Zedan <zed.saheer5@gmail.com>
2022-07-22 18:49:01 +0530

    Created type for channels in a guild

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -11,6 +11,34 @@ export type Guild = {
     features: string[];
 }
 
+export type Channel = {
+    id: string;
+    type: number;
+    guild_id: string;
+    name: string;
+    permission_overwrites: Overwrite[];
+    position: number;
+    flags: number;
+    bitrate: number;
+    parent_id: string;
+    topic: string;
+    user_limit: number;
+    rate_limit_per_user: number;
+    nsfw: boolean;
+    message_count: number;
+    member_count: number;
+    rtc_region: string;
+    total_message_sent: number;
+    last_message_id: string;
+}
+
+export type Overwrite = {
+    id:string;
+    type:number;
+    allow:string;
+    deny:string;
+}
+
 export type NextPageWithLayout<T> = NextPage<T> & {
     //The next page type with an optional get layout function that returns a page
     getLayout?: (page: ReactElement) => ReactNode;
